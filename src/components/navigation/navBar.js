5fcbf39e5acd510dd629ce84b941461310376ac0
Adrian Mostowski <adaxelx@o2.pl>
2019-05-28 22:56:42 +0200

    add nav files

---
+++ src/components/navigation/navBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Navigation.module.sass'
+
+class NavBar extends React.Component{
+    render(){
+        return (
+            <div className={styles.nav}>
+
+            </div>
+         );
+    }
+}
+
+export default NavBar;
