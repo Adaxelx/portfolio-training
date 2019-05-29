73dfd35e43fb915bba792251993ece842236b942
Adrian Mostowski <adaxelx@o2.pl>
2019-05-29 13:11:35 +0200

    add navBar styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import NavBar from './components/navigation/navBar.js'
 
 function App() {
   return (
     <div className="App">
-
+        <NavBar/>
     </div>
   );
 }
--- src/components/navigation/navBar.js
+++ src/components/navigation/navBar.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import styles from './Navigation.module.sass'
+import styles from '../../Styles/Modules/Navigation.module.sass'
 
 class NavBar extends React.Component{
     render(){
         return (
             <div className={styles.nav}>
-
+                <span className={`${styles.bar} ${styles.top}`}></span>
+                <span className={`${styles.bar} ${styles.mid}`}></span>
+                <span className={`${styles.bar} ${styles.bot}`}></span>
             </div>
          );
     }
