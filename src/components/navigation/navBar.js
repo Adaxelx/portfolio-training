32bdaae0a5ddf4d61fcdfaa0834505e6fea66e48
Adrian Mostowski <adaxelx@o2.pl>
2019-05-29 13:54:15 +0200

    add animation on bars + fontawansome

--- src/components/navigation/navBar.js
+++ src/components/navigation/navBar.js
@@ -2,12 +2,23 @@ import React from 'react';
 import styles from '../../Styles/Modules/Navigation.module.sass'
 
 class NavBar extends React.Component{
+    state = {
+        active: false
+    }
+
+    handleClick = () =>{
+        this.setState({
+            active: !this.state.active
+        })
+    }
+
     render(){
         return (
-            <div className={styles.nav}>
-                <span className={`${styles.bar} ${styles.top}`}></span>
-                <span className={`${styles.bar} ${styles.mid}`}></span>
-                <span className={`${styles.bar} ${styles.bot}`}></span>
+            <div className={`${styles.nav} ${this.state.active ? styles.active : ''}`} onClick={this.handleClick}>
+                    <div className={`${styles.bars} ${this.state.active ? styles.active : ''}`}>
+                    <span className={`${styles.bar} ${styles.top} ${this.state.active ? styles.active : ''}`}></span>
+                    <span className={`${styles.bar} ${styles.bot} ${this.state.active ? styles.active : ''}`}></span>
+                </div>
             </div>
          );
     }
