5f912aa598c51988b9d293ac1885f6075368fbfa
Adrian Mostowski <adaxelx@o2.pl>
2019-05-30 15:58:22 +0200

    add working animations + socials

--- src/components/navigation/navOpen.js
+++ src/components/navigation/navOpen.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import styles from '../../Styles/Modules/OpenedNav.module.sass'
 import {NavLink} from 'react-router-dom'
+
 class navOpen extends React.Component {
     state = {  }
+
     render(props) {
         return (
-            <nav className={`${styles.nav} ${this.props.active ? styles.active : ''}`}>
+            <nav className={`${styles.nav} ${this.props.active ? styles.open : ''}`}>
                 <div className={styles.content}>
                     <ul className={styles.list}>
-                        <NavLink to='/'><li>Start</li></NavLink>
-                        <NavLink to='/about'><li>O mnie</li></NavLink>
-                        <NavLink to='/contact'><li>Kontakt</li></NavLink>
+                        <NavLink to='/' className={`${styles.link}`}>
+                            <li className={`${this.props.active ? styles.active : ''}`}>Start</li></NavLink>
+                        <NavLink to='/about' className={`${styles.link}`}>
+                            <li className={`${styles.second} ${this.props.active ? styles.active : ''}`}>O mnie</li></NavLink>
+                        <NavLink to='/contact' className={`${styles.link}`}>
+                            <li className={`${styles.third} ${this.props.active ? styles.active : ''}`}>Kontakt</li></NavLink>
                     </ul>
+                    <aside className={`${styles.socials}`}>
+                        <p className={`${styles.text} ${this.props.active ? styles.active : ''}`}>Jeżeli chcesz dowiedzieć się o mnie więcej zapraszam na moje media społecznościowe i githuba</p>
+                        <i className={`fab fa-github ${this.props.active ? styles.active : ''}`}></i>
+                        <i className={`fab fa-facebook-f ${this.props.active ? styles.active : ''}`}></i>
+                        <i className={`fab fa-instagram ${this.props.active ? styles.active : ''}`}></i>
+                    </aside>
                 </div>
             </nav>
          );
