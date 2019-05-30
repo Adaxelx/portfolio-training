1d345c7163584d70aa074de4f40e69fcec3f6524
Adrian Mostowski <adaxelx@o2.pl>
2019-05-30 18:11:54 +0200

    add about files

---
+++ src/components/StartPage/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../../Styles/Modules/StartPage/About.module.sass'
+
+const About = () => {
+    return (
+        <section></section>
+     );
+}
+
+export default About;
