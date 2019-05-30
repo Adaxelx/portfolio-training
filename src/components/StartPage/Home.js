0db15a7fd5037688267a2c82806857113c69094c
Adrian Mostowski <adaxelx@o2.pl>
2019-05-30 17:41:35 +0200

    full home page with styles

--- src/components/StartPage/Home.js
+++ src/components/StartPage/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import img from '../../images/mostoski.png'
+import styles from '../../Styles/Modules/StartPage/Home.module.sass'
 const Home = () => {
     return (
-        <section>
-            <img src={img} alt=""/>
+        <section className={styles.home}>
+            <h1 className={styles.title}>Adrian Mostowski</h1>
+            <h2 className={styles.subtitle}>- Front-End Developer</h2>
+            <div className={styles.bg}>
+            <img src={img} alt="to ja" className={styles.img}/>
+            </div>
         </section>
      );
 }
