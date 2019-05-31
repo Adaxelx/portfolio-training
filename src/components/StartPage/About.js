10f53666cf88ee4d4478e71e72f72fa7745864c2
Adrian Mostowski <adaxelx@o2.pl>
2019-05-31 13:07:04 +0200

    add conent to about and scrolling to startPage

--- src/components/StartPage/About.js
+++ src/components/StartPage/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from '../../Styles/Modules/StartPage/About.module.sass'
-
+import img from '../../images/Tkd.jpg'
 let scrollV = window.scrollY
 class About extends React.Component{
     state = {
@@ -37,7 +37,17 @@ class About extends React.Component{
     const {active} = this.state
     return (
         <article ref={section} className={`${styles.about} ${active ? styles.active : ''}`}>
-            <section className={styles.container}><h2 ref={title}className={styles.title}>O mnie</h2></section>
+            <section className={styles.container}>
+                <h2 ref={title}className={styles.title}>O mnie</h2>
+            </section>
+            <section className={styles.content}>
+                <img src={img} alt="ME2018"/>
+                <p>Nazywam się Adrian Mostowski i jestem maturzystą. Moimi pasjami są webdevelopment i sztuka walki Taekwon-Do</p>
+
+                <p>Samym webdevelpmentem zainteresowałem się na początku liceum. Jednak przez brak pomysłów wróciłem do niego dopiero w wakacje 2018 roku. Od tamtej pory regularnie przerabiam kursy i piszę własne projekty oraz staram się rozwijać, aby mój kod był jak najczystszy.</p>
+
+                <p>Aktualnie większość moich projektów jest oparta na Reactcie, ale zwykły HTML czy JS również nie jest mi obcy co można zobaczyć w projektach poniżej.</p>
+            </section>
         </article>
      );
     }
--- src/components/StartPage/StartPage.js
+++ src/components/StartPage/StartPage.js
@@ -3,6 +3,11 @@ import Home from './Home.js'
 import About from './About.js'
 class StarPage extends React.Component {
     state = {  }
+
+    componentDidMount(){
+        window.scrollTo(0,0)
+    }
+
     render() {
         return (
         <article>
