bd0920511e7512ce49b0310a7a56fdeb066ce459
Adrian Mostowski <adaxelx@o2.pl>
2019-05-31 13:15:44 +0200

    add skills files

---
+++ src/components/StartPage/Skills.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../../Styles/Modules/StartPage/Skills.module.sass'
+class Skills extends React.Component {
+    state = {  }
+    render() {
+        return (
+        <article></article>
+        );
+    }
+}
+
+export default Skills;
--- src/components/StartPage/StartPage.js
+++ src/components/StartPage/StartPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Home from './Home.js'
 import About from './About.js'
+import Skills from './Skills.js'
 class StarPage extends React.Component {
     state = {  }
 
@@ -13,6 +14,7 @@ class StarPage extends React.Component {
         <article>
             <Home/>
             <About/>
+            <Skills/>
         </article> );
     }
 }
