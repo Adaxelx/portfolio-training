af43136835176c4fc4d920d3689d57c86bbc87b5
Adrian Mostowski <adaxelx@o2.pl>
2019-05-31 12:36:05 +0200

    add title to about with animation

--- src/components/StartPage/About.js
+++ src/components/StartPage/About.js
@@ -1,10 +1,47 @@
 import React from 'react';
 import styles from '../../Styles/Modules/StartPage/About.module.sass'
 
-const About = () => {
+let scrollV = window.scrollY
+class About extends React.Component{
+    state = {
+        active: false,
+    }
+
+    handleScroll = () =>{
+        const {section, title} = this
+        scrollV = window.scrollY
+        if(scrollV>section.current.offsetTop - window.innerHeight + 200){
+            this.setState({
+                active: true
+            })
+        }
+        else{
+            this.setState({
+                active: false
+            })
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll)
+    }
+    componentWillUnmount(){
+        window.removeEventListener('scroll', this.handleScroll)
+    }
+
+    section = React.createRef();
+    title = React.createRef()
+
+    render(){
+    const {section,title} = this
+    const {active} = this.state
     return (
-        <section></section>
+        <article ref={section} className={`${styles.about} ${active ? styles.active : ''}`}>
+            <section className={styles.container}><h2 ref={title}className={styles.title}>O mnie</h2></section>
+        </article>
      );
+    }
+
 }
 
 export default About;
--- src/components/StartPage/StartPage.js
+++ src/components/StartPage/StartPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Home from './Home.js'
+import About from './About.js'
 class StarPage extends React.Component {
     state = {  }
     render() {
         return (
         <article>
             <Home/>
+            <About/>
         </article> );
     }
 }
