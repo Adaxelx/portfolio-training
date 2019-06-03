7a8684fcdfb14f9e75003f4142e3128826ea30d6
Adrian Mostowski <adaxelx@o2.pl>
2019-06-03 16:47:51 +0200

    add target blank to a

--- src/components/StartPage/Projects.js
+++ src/components/StartPage/Projects.js
@@ -48,17 +48,17 @@ class Projects extends React.Component {
                 <section className={styles.project}>
                     <img src={img1} alt=""/>
                     <p>Jest to projekt salonu fryzjerskiego. Jest to strona oparta na reactcie wraz z użytmi m.in react-routerem czy react-transition-group, które pomogły upiększyć przejścia między poszczególnymi podstronami </p>
-                    <a href="https://adaxelx.github.io/strona-salonu/"><button className={styles.button}><i className="fab fa-github"></i></button></a>
+                    <a target="_blank" href="https://adaxelx.github.io/strona-salonu/"><button className={styles.button}><i className="fab fa-github"></i></button></a>
                 </section>
                 <section className={styles.project}>
                     <img src={img2} alt=""/>
                     <p>Jest to szablon strony opartej na reactcie. Dodatkiem do niej jest react - router oraz kilka ciekawych animacji</p>
-                    <a href="https://adaxelx.github.io/react-react-router-2/"><button className={styles.button}><i className="fab fa-github"></i></button></a>
+                    <a target="_blank" href="https://adaxelx.github.io/react-react-router-2/"><button className={styles.button}><i className="fab fa-github"></i></button></a>
                 </section>
                 <section className={styles.project}>
                     <img src={img3} alt=""/>
                     <p>Jeden z moich pierwszych projektów opartych na reactcie. Jest w nim jednak kilka ciekawych rozwiązań i animacji, które warto zobaczyć.</p>
-                    <a href="https://adaxelx.github.io/react-with-router/"><button className={styles.button}><i className="fab fa-github"></i></button></a>
+                    <a target="_blank" href="https://adaxelx.github.io/react-with-router/"><button className={styles.button}><i className="fab fa-github"></i></button></a>
                 </section>
             </article>
         );
