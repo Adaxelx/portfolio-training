89e32e2eb5d00cee6886322c6fb0f71972b46771
Adrian Mostowski <adaxelx@o2.pl>
2019-06-03 14:45:37 +0200

    add Projects files

---
+++ src/components/StartPage/Projects.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from '../../Styles/Modules/StartPage/Projects.module.sass'
+
+class Projects extends React.Component {
+    state = {  }
+    render() {
+        return (
+            <article className={styles.container}>
+
+            </article>
+        );
+    }
+}
+
+export default Projects;
