c09f28be84aaefa9c33fb3bb6a9d954fef974de3
Adrian Mostowski <adaxelx@o2.pl>
2019-06-05 12:12:49 +0200

    add all views to about

--- src/components/StartPage/About.js
+++ src/components/StartPage/About.js
@@ -42,11 +42,14 @@ class About extends React.Component{
             </section>
             <section className={styles.content}>
                 <img src={img} alt="ME2018"/>
+
+                <section className={styles.text}>
                 <p>Nazywam się Adrian Mostowski i jestem maturzystą. Moimi pasjami są webdevelopment i sztuka walki Taekwon-Do</p>
 
                 <p>Samym webdevelpmentem zainteresowałem się na początku liceum. Jednak przez brak pomysłów wróciłem do niego dopiero w wakacje 2018 roku. Od tamtej pory regularnie przerabiam kursy i piszę własne projekty oraz staram się rozwijać, aby mój kod był jak najczystszy.</p>
 
                 <p>Aktualnie większość moich projektów jest oparta na Reactcie, ale zwykły HTML czy JS również nie jest mi obcy co można zobaczyć w projektach poniżej.</p>
+                </section>
             </section>
         </article>
      );
