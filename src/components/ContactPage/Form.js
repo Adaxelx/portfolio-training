ea789027bf4f5a2d86b2eae236a199a29bd66562
Adrian Mostowski <adaxelx@o2.pl>
2019-07-02 00:05:09 +0200

    add some content to form on mobile

--- src/components/ContactPage/Form.js
+++ src/components/ContactPage/Form.js
@@ -4,8 +4,9 @@ import styles from '../../Styles/Modules/ContactPage/Form.module.sass'
 class Form extends React.Component {
     state = {
         username: '',
-        number: '',
-        message:'',
+        company: '',
+        reason: '',
+        message: '',
      }
 
      handleChange = (e) =>{
@@ -20,9 +21,18 @@ class Form extends React.Component {
         return (
             <form action="" className={styles.form}>
                 <h3 className={styles.title}>Opowiedz swoją historię: </h3>
-                <label className='story' htmlFor="story">
-                    <p className={styles.content}>Nazywam się </p><input placeholder='Imię i nazwisko' type="text" id="user" name="username" value={this.state.username} onChange={this.handleChange}></input>
-                    <p></p>
+                <label className={styles.container} htmlFor="user">
+                    <p className={styles.content}>Nazywam się </p><input className={styles.input} placeholder='imię i nazwisko' type="text" id="user" name="username" value={this.state.username} onChange={this.handleChange}></input>
+                </label>
+                <label className={styles.container} htmlFor="user">
+                    <p className={styles.content}>Pracuje dla firmy</p><input className={styles.input} placeholder='nazwa firmy' type="text" id="user" name="company" value={this.state.company} onChange={this.handleChange}></input>
+                </label>
+                <label className={styles.container} htmlFor="user">
+                    <p className={styles.content}>Powód wiadomości</p><input className={styles.input} placeholder='powód' type="text" id="user" name="reason" value={this.state.reason} onChange={this.handleChange}></input>
+                </label>
+                <label className='message' htmlFor="message">
+                    <p className={styles.content}>Napisz coś więcej</p>
+                    <textarea className={styles.message} id="message" name="message" value={this.state.message} placeholder='nie krępuj się' onChange={this.handleChange}></textarea>
                 </label>
             </form>
         );
