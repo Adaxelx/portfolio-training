8d6fc54ea2bf4b29875b5e5d6f5123e7a5166ad3
Adrian Mostowski <adaxelx@o2.pl>
2019-07-02 11:01:34 +0200

    add more options in form

--- src/components/ContactPage/Form.js
+++ src/components/ContactPage/Form.js
@@ -7,6 +7,8 @@ class Form extends React.Component {
         company: '',
         reason: '',
         message: '',
+        email: '',
+        number: ''
      }
 
      handleChange = (e) =>{
@@ -30,9 +32,13 @@ class Form extends React.Component {
                 <label className={styles.container} htmlFor="user">
                     <p className={styles.content}>Powód wiadomości</p><input className={styles.input} placeholder='powód' type="text" id="user" name="reason" value={this.state.reason} onChange={this.handleChange}></input>
                 </label>
-                <label className='message' htmlFor="message">
+                <label className={styles.container} htmlFor="message">
                     <p className={styles.content}>Napisz coś więcej</p>
                     <textarea className={styles.message} id="message" name="message" value={this.state.message} placeholder='nie krępuj się' onChange={this.handleChange}></textarea>
+                </label>
+                <label className={styles.container} htmlFor="user">
+                    <p className={styles.content}>Jak się z tobą skontaktować?</p><input className={styles.input} placeholder='email' type="e-mail" id="user" name="email" value={this.state.email} onChange={this.handleChange}></input>
+                    <input className={styles.input} placeholder='numer telefonu' type="number" id="user" name="number" value={this.state.number} onChange={this.handleChange}></input>
                 </label>
             </form>
         );
