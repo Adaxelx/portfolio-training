889679c8e4a496682ac6561c73e46199a0d03d45
Adrian Mostowski <adaxelx@o2.pl>
2019-07-02 23:26:03 +0200

    Updates

---
+++ precache-manifest.836467e103280baa74f25ddf57f99b1e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24a568e8cebbcc9b0aefe9bc2a2c0c01",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c7640ade505c9471a618",
+    "url": "/portfolio/static/css/main.0d11695d.chunk.css"
+  },
+  {
+    "revision": "40364430d46edec0c8e7",
+    "url": "/portfolio/static/js/2.0e8627dd.chunk.js"
+  },
+  {
+    "revision": "c7640ade505c9471a618",
+    "url": "/portfolio/static/js/main.2827037b.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "dd5079a84fbecebbae0576b6dbf8d72c",
+    "url": "/portfolio/static/media/React.dd5079a8.png"
+  },
+  {
+    "revision": "5346cf420e78ff01caffcf99fde134fc",
+    "url": "/portfolio/static/media/Tkd.5346cf42.jpg"
+  },
+  {
+    "revision": "b626873e6254e2f819c76b77364d87b8",
+    "url": "/portfolio/static/media/css.b626873e.png"
+  },
+  {
+    "revision": "ee1369e96e441ad0119530d10264b8b4",
+    "url": "/portfolio/static/media/git.ee1369e9.png"
+  },
+  {
+    "revision": "d656adeafd5c021748fc5047be709a7a",
+    "url": "/portfolio/static/media/js.d656adea.png"
+  },
+  {
+    "revision": "b1f1861cdb5bfe9a1df4e37cd5bf4528",
+    "url": "/portfolio/static/media/mostoski.b1f1861c.png"
+  },
+  {
+    "revision": "6dcf699938b2291d1634f91adb0dded1",
+    "url": "/portfolio/static/media/nodejslogo.6dcf6999.png"
+  },
+  {
+    "revision": "3b8b3dc74effd77d9888e9ab1db6e51d",
+    "url": "/portfolio/static/media/react-router-2.3b8b3dc7.png"
+  },
+  {
+    "revision": "c7f25b9a4e8ed8758b887ff71a715d0f",
+    "url": "/portfolio/static/media/react-router.c7f25b9a.png"
+  },
+  {
+    "revision": "8f34ff7d71a85cdc212599d72583bd96",
+    "url": "/portfolio/static/media/salon.8f34ff7d.png"
+  },
+  {
+    "revision": "2f37751167dda952861983c6c86af543",
+    "url": "/portfolio/static/media/windows.2f377511.png"
+  }
+]);
